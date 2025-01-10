acc8fc2a02224e5e5fe7f723c6e1ffeba70d7d09
antonioferrigno28 <ferrigno1@hotmail.it>
2025-01-10 16:32:01 +0100

    index/show done!

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -13,7 +13,34 @@ function index(req, res) {
   });
 }
 
-const show = (req, res) => {
-  res.send(`Dettagli del film con ID: ${req.params.id}`);
-};
+function show(req, res) {
+  const id = parseInt(req.params.id);
+  const sql = "SELECT * FROM `movies` WHERE `id` = ?";
+
+  connection.query(sql, [id], (err, moviesResults) => {
+    if (err) {
+      console.log(err);
+      return res
+        .status(500)
+        .json({ error: "La query non è andata a buon fine" });
+    }
+    if (moviesResults.length === 0) {
+      return res.status(404).json({ error: "Post non trovato" });
+    }
+
+    const selectedMovie = moviesResults[0];
+    const reviewsQuery = "SELECT * FROM reviews WHERE movie_id = ?";
+    connection.query(reviewsQuery, [id], (err, reviewsResults) => {
+      if (err) {
+        console.error("Errore durante la ricerca delle recensioni", err);
+        return res.status(500).send("Errore del server");
+      }
+
+      res.json({
+        selectedMovie,
+        reviews: reviewsResults,
+      });
+    });
+  });
+}
 module.exports = { index, show };
